ae14aa8858bf9b726d1b3719f95de10f8ddb4bb9
TamaoSan <thanakitmoss@gmail.com>
2024-02-26 02:14:47 +0700

    Update Controllers of companies and add MongoDB

--- controllers/companies.js
+++ controllers/companies.js
@@ -1,34 +1,75 @@
+const Company = require('../models/Companies');
 //@desc     Get all companies
 //@route    GET /api/v1/hospitals
 //@access   Public
-exports.getCompanies=(req,res,next)=>{
-    res.status(200).json({success:true, msg:'Show all companies'});
+exports.getCompanies=async(req,res,next)=>{
+    try{
+        const company = await Company.find();
+
+        res.status(200).json({success:true, count:company.length, data:company});
+    } catch(err){
+        res.status(400).json({success:false});
+    }
 };
 
 //@desc     Get single company
 //@route    GET /api/v1/companies/:id
 //@access   Public
-exports.getCompany=(req,res,next)=>{
-    res.status(200).json({success:true, msg:`Show company ${req.params.id}`});
+exports.getCompany=async(req,res,next)=>{
+    try{
+        const company = await Company.findById(req.params.id);
+
+        if(!company){
+            return res.status(400).json({success:false});
+        }
+
+        res.status(200).json({success:true, data:company});
+    } catch(err){
+        res.status(400).json({success:false});
+    }
 };
 
 //@desc     Create new company
 //@route    POST /api/v1/companies
 //@access   Public
-exports.createCompany=(req,res,next)=>{
-    res.status(200).json({success:true, msg:'Create new companies'});
+exports.createCompany=async(req,res,next)=>{
+    const company = await Company.create(req.body);
+    res.status(201).json({success:true, data:company});
 };
 
 //@desc     Update company
 //@route    PUT /api/v1/companies/:id
 //@access   Public
-exports.updateCompany=(req,res,next)=>{
-    res.status(200).json({success:true, msg:`Update company ${req.params.id}`});
+exports.updateCompany=async(req,res,next)=>{
+    try{
+        const company = await Company.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators:true
+        });
+
+        if(!Company){
+            return rawListeners.status(400).json({success:false});
+        }
+
+        res.status(200).json({success:true, data:company});
+    } catch(err){
+        res.status(400).json({success:false});
+    }
 };
 
 //@desc     Delete company
 //@route    DELETE /api/v1/companies/:id
 //@access   Public
-exports.deleteCompany=(req,res,next)=>{
-    res.status(200).json({success:true, msg:`Delete company ${req.params.id}`});
+exports.deleteCompany=async(req,res,next)=>{
+    try{
+        const company = await Company.findByIdAndDelete(req.params.id);
+
+        if(!company){
+            return res.status(400).json({success:false});
+        }
+
+        res.status(200).json({success:true, data:{}});
+    }catch(err){
+        res.status(400).json({success:false});
+    }
 };
--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 //Route files
 const companies = require('./routes/companies');
-
 //Load env vars
 dotenv.config({path:'./config/config.env'});
+//Connecct to database
+connectDB();
 
 const app=express();
+
+//add body parser
+app.use(express.json());
 
 //Mount routers
 app.use('/api/v1/companies',companies);
 
 const PORT=process.env.PORT || 5000;
-app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection',(err,promise)=>{
+    console.log(`Error: ${err.message}`);
+    //Close server & exit process
+    server.close(()=>process.exit(1));
+});
