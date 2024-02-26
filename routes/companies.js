01308ea2e3286d55d7944b56f21e665f5bffc832
TamaoSan <thanakitmoss@gmail.com>
2024-02-26 18:15:16 +0700

    auth protect added

--- routes/companies.js
+++ routes/companies.js
@@ -3,7 +3,9 @@ const {getCompanies, getCompany, createCompany, updateCompany, deleteCompany} = 
 
 const router = express.Router();
 
-router.route('/').get(getCompanies).post(createCompany);
-router.route('/:id').get(getCompany).put(updateCompany).delete(deleteCompany);
+const {protect} = require('../middleware/auth');
+
+router.route('/').get(getCompanies).post(protect,createCompany);
+router.route('/:id').get(getCompany).put(protect,updateCompany).delete(protect,deleteCompany);
 
 module.exports=router;
