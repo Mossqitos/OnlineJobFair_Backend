1a7b609a113e945602cacdee24af7644a0a728e2
TamaoSan <thanakitmoss@gmail.com>
2024-02-28 03:07:45 +0700

    Add Interview

--- controllers/interviews.js
+++ controllers/interviews.js
@@ -1,4 +1,5 @@
 const Interview = require('../models/Interview');
+const Company = require('../models/Companies')
 
 //@desc     Get all interviews
 //@route    GET /api/v1/interviews
@@ -27,5 +28,53 @@ exports.getInterviews=async(req,res,next)=>{
     } catch(err){
         console.log(err.stack);
         return req.status(500).json({success:false,message:"Cannot find Interview"});
+    }
+};
+
+//@desc     Get single interview
+//@route    GET /api/v1/interviews/:id
+//@access   Public
+exports.getInterview=async(req,res,next)=>{
+    try{
+        const interview= await Interview.findById(req.params.id).populate({
+            path: 'company',
+            select: 'name description tel'
+        });
+        
+        if(!interview){
+            return res.status(404).json({success:false,message:`No interview with the id od ${req.params.id}`});
+        }
+
+        res.status(200).json({
+            success:true,
+            data:interview
+        });
+    } catch(err){
+        console.log(err.stack);
+        return res.status(500).json({success:false,message:"Cannot find Interview"});
+    }
+};
+
+//@desc     Add interview
+//@route    POST /api/v1/companies/:companyId/interview
+//@access   Private
+exports.addInterview=async(req,res,next)=>{
+    try{
+        req.body.company=req.params.companyId;
+
+        const companies= await Company.findById(req.params.companyId);
+
+        if(!companies){
+            return res.status(404).json({success:false,message:`No hospital with the id of ${req.params.companyId}`});
+        }
+        const interview = await Interview.create(req.body);
+
+        res.status(200).json({
+            success:true,
+            data:interview
+        });
+    } catch(err){
+        console.log(err.stack);
+        return res.status(500).json({success:false,message:"Cannot create Interview"});
     }
 };
--- routes/interviews.js
+++ routes/interviews.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const {getInterviews} = require('../controllers/interviews');
+const {getInterviews, getInterview, addInterview} = require('../controllers/interviews');
 
 const router = express.Router({mergeParams:true});
 
 const {protect}= require('../middleware/auth');
 
-router.route('/').get(protect, getInterviews);
+router.route('/').get(protect, getInterviews).post(addInterview);
+router.route('/:id').get(getInterview);
 
 module.exports=router;
