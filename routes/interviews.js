48626369551c8857e811bf1ccd6914cd8444b4eb
TamaoSan <thanakitmoss@gmail.com>
2024-02-28 14:53:15 +0700

    Protected Routes

--- routes/interviews.js
+++ routes/interviews.js
@@ -3,9 +3,9 @@ const {getInterviews, getInterview, addInterview, updateInterview, deleteIntervi
 
 const router = express.Router({mergeParams:true});
 
-const {protect}= require('../middleware/auth');
+const {protect,authorize}= require('../middleware/auth');
 
-router.route('/').get(protect, getInterviews).post(addInterview);
-router.route('/:id').get(getInterview).put(updateInterview).delete(deleteInterview);
+router.route('/').get(protect, getInterviews).post(protect,authorize('admin','user'), addInterview);
+router.route('/:id').get(protect, getInterview).put(protect,authorize('admin','user'), updateInterview).delete(protect,authorize('admin','user'), deleteInterview);
 
 module.exports=router;
