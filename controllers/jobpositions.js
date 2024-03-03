122fad2a274fe1e28fa3b9ad4ad4e5daccde1150
TamaoSan <thanakitmoss@gmail.com>
2024-03-03 19:02:59 +0700

    API security

--- controllers/jobpositions.js
+++ controllers/jobpositions.js
@@ -42,7 +42,7 @@ exports.createJobposition=async(req,res,next)=>{
 
         const jobposition = await Jobposition.create(req.body);
 
-        res.status(200).json({
+        res.status(201).json({
             success:true,
             data:jobposition
         });
--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const companies = require('./routes/companies');
 const auth = require('./routes/auth');
 const interviews =require('./routes/interviews');
 const jobposition = require('./routes/jobposition');
+const mongoSanitize=require('express-mongo-sanitize');
+const helmet=require('helmet');
+const {xss}=require('express-xss-sanitizer');
+const rateLimit=require('express-rate-limit');
+const hpp=require('hpp');
+const cors=require('cors');
 //Load env vars
 dotenv.config({path:'./config/config.env'});
 //Connecct to database
@@ -19,6 +25,28 @@ app.use(express.json());
 
 //Cookie parser
 app.use(cookieParser());
+
+//Sanitize data
+app.use(mongoSanitize());
+
+//Set security headers
+app.use(helmet());
+
+//Prevent XSS attacks
+app.use(xss());
+
+//Prevent http param pollutions
+app.use(hpp());
+
+//Enable CORS
+app.use(cors());
+
+//Rate Limiting
+const limiter=rateLimit({
+    windowMs:10*60*1000,//10 mins
+    max: 100
+});
+app.use(limiter);
 
 //Mount routers
 app.use('/api/v1/companies',companies);
